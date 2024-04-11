7dabd6a04d82fbf5d9f59cea8d9c8f0967022a1f
Ogulcan O <88856243+math349@users.noreply.github.com>
2024-04-12 01:21:12 +0300

    Update funcApp_Legendre.py

--- FunctionApproximation/funcApp_Legendre.py
+++ FunctionApproximation/funcApp_Legendre.py
@@ -9,7 +9,7 @@ def f(x): # the function to approximate
 X = np.linspace(-1, 1, 100) # The domain of the Legendre polynomials for "Orthogonality"
 for p in range(6,10,3): # Legendre approximations for different p "degree of polynomials"
     roots, weights = roots_legendre(p) # Roots and weights of Legendre Polynomials
-    coeff = np.zeros(p) # Computations of the legendre coefficients
+    coeff = np.zeros(p) # Computations of the Legendre coefficients
     for j in range(p):
         I = 0
         for i in range(len(weights)):
